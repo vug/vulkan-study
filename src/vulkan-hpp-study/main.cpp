b3ca1525773c3ea4b2d271dcadcb3d4b3132fe0b
Uğur Güney <ugurguney@gmail.com>
2022-10-19 00:23:10 -0400

    Use vulkan_raii and utils to create instance and enumerate physics devices

--- src/vulkan-hpp-study/main.cpp
+++ src/vulkan-hpp-study/main.cpp
@@ -1,6 +1,24 @@
+#include "utils.hpp"
+
+#include <vulkan/vulkan_raii.hpp>
+
 #include <iostream>
+#include <string>
+
+static std::string AppName = "01_InitInstanceRAII";
+static std::string EngineName = "Vulkan.hpp";
 
 int main() {
 	std::cout << "Hello, Vulkan!\n";
+
+	vk::raii::Context context;
+	vk::ApplicationInfo applicationInfo(AppName.c_str(), 1, EngineName.c_str(), 1, VK_API_VERSION_1_3);
+	vk::raii::Instance instance = vk::raii::su::makeInstance(context, AppName, EngineName);
+	vk::raii::PhysicalDevices physicalDevices(instance);
+	// simple test that physical device found
+	auto features = physicalDevices.front().getFeatures();
+	std::cout << "numDevices: " << physicalDevices.size() << ", physicalDevice[0] has multi-viewport: " << features.multiViewport << '\n';
+
+	std::cout << "Bye, Vulkan!\n";
 	return 0;
 }
