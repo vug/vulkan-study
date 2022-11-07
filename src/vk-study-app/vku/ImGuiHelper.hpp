346165dd3b6050f4bdf05b6773f9def20b7c560e
Uğur Güney <ugurguney@gmail.com>
2022-11-07 01:05:18 -0500

    ImGuiHelper class to make ImGui integration with the app easier

---
+++ src/vk-study-app/vku/ImGuiHelper.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <vulkan/vulkan.h>
+
+namespace vku {
+	class VulkanContext;
+	class Window;
+
+	class ImGuiHelper {
+	public:
+		ImGuiHelper(const VulkanContext& vc, const Window& win);
+		~ImGuiHelper();
+		void Begin() const;
+		void End() const;
+
+		void AddDrawCalls(const VkCommandBuffer& cmdBuf) const;
+	private:
+		const VulkanContext& vc;
+		const Window& win;
+		VkDescriptorPool imguiPool;
+	};
+}
